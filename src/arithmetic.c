d20b57ea0c63bee9bd575178aa3cab90f28d6735
Kwaltz Buck <kwaltzbu@cl-a1.nsk.21-school.ru>
2022-07-27 16:32:21 +0700

    aritmetic

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 
 int sum(int a, int b);
-
+int dif(int a, int b);
+int mult(int a, int b);
+int divv(int a, int b);
 int main()
 {
-    return 0;
+    float a1 = 0, b1 = 0;
+    int check = scanf("%f %f",&a1 ,&b1);
+    int a = (int)a1;
+    int b = (int)b1;
+    
+    if (a1 - a != 0 || b - b1 != 0 || check != 2)
+        printf("n/a");
+    else {
+        printf("%d %d %d ", sum(a,b), dif(a,b), mult(a,b) );
+        if ((int)b == 0)
+            printf("n/a");
+        else
+            printf("%d", divv(a,b));
+    }
+	return 0;
 }
 
 int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+    return (a + b);
 }
+int dif(int a, int b){
+    return (a - b);
+}
+int mult(int a, int b){
+    return (a * b);
+}
+int divv(int a, int b){
+    if (b !=0 ){
+        return (a / b);
+    }
+    else 
+        return a;
+}
+
---
+++ src/named_hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int name = 0;
+   printf("Введите число в качестве имени:");
+   while (scanf("%d",&name) != 1){
+		printf("\nОшибка ввода. Попробуйте снова:");
+   }	
+   printf("Hello, %d!", name);
+   return 0;
+}
+
